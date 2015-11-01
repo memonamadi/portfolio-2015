88ab8967d849643d168128a895b2927e36e3cf38
memonamadi <madi.memona@gmail.com>
2015-11-01 19:21:35 +0000

    last styles updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,7 @@
 
 	function init(){
 		showNav();
+		toSection();
 	}
 
 	function showNav(){
@@ -15,6 +16,27 @@
 			$(nav_container).slideToggle('slow').toggleClass('active');
 		});	
 	}
+
+	function toSection() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			$(nav_container).hide().removeClass('active');
+			$(hamburger_icon).removeClass('open');
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+		        || location.hostname == this.hostname) {
+
+		        var target = $(this.hash);
+		        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		           if (target.length) {
+		             $('html,body').animate({
+		                 scrollTop: target.offset().top
+		            }, 1000);
+		            return false;
+		        }
+		    }
+		});
+	}
+
+	
 
 
 	init();
